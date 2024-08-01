a050e081590af99c6561049f06a1f7371794e3d1
Kei-Oh <acht542@gmail.com>
2024-08-01 15:09:28 +0900

    googleフォント追加

---
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+import { Noto_Sans_Javanese } from 'next/font/google';
+
+export const notosans_jp = Noto_Sans_Javanese({
+  subsets: ['latin'],
+  weight: '500',
+});
